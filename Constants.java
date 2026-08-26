fdfc53db37c0727acda15df930a1bf30610c9d2b
ZRJohnson <zachrjohnson08@gmail.com>
2026-08-26 14:32:34 -0400

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -18,17 +18,25 @@ public class Constants {
 
   // Private constructor to prevent unnecessary instantiation
   private Constants() {}
-
-  // Hardware device name strings
-  public static String MOTOR_FRONT_LEFT  = "fl";
-  public static String MOTOR_FRONT_RIGHT = "fr";
-  public static String MOTOR_BACK_LEFT   = "bl";
-  public static String MOTOR_BACK_RIGHT  = "br";
-
-  // Robot physical & tuning parameters
-  public static double COUNTS_PER_ROTATION   = 537.7;
-  public static double WHEEL_DIAMETER_INCHES = 3.779;
-  public static double DRIVE_GEAR_REDUCTION  = 1.0;
-  static final double  COUNTS_PER_INCH       = (COUNTS_PER_ROTATION * DRIVE_GEAR_REDUCTION) 
+  
+  /**
+   * Hardware device names.
+   */
+  public static class Hardware {
+    public static String MOTOR_FRONT_LEFT  = "fl";
+    public static String MOTOR_FRONT_RIGHT = "fr";
+    public static String MOTOR_BACK_LEFT   = "bl";
+    public static String MOTOR_BACK_RIGHT  = "br";
+  }
+  
+  /**
+   * Robot physical dimensions and drivetrain parameters
+   */
+  public static class Drive {
+    public static double COUNTS_PER_ROTATION   = 537.7;
+    public static double WHEEL_DIAMETER_INCHES = 3.779;
+    public static double DRIVE_GEAR_REDUCTION  = 1.0;
+    public static double COUNTS_PER_INCH       = (COUNTS_PER_ROTATION * DRIVE_GEAR_REDUCTION) 
                                                / (WHEEL_DIAMETER_INCHES * Math.PI);
+  }
 }
